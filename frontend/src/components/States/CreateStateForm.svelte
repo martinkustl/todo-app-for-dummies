c8b2b916ee064d48e071b549a6b1bbb10b086216
skepter <48717111+skepter@users.noreply.github.com>
2022-03-22 17:33:28 +0100

    style(main): fix formatting

--- frontend/src/components/States/CreateStateForm.svelte
+++ frontend/src/components/States/CreateStateForm.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
-    import type {State} from '../../types';
-    import {apiBaseUrl, http} from '../../common';
-    import Input from '../shared/Inputs/Input.svelte';
-    import CreateRecordForm from '../shared/Forms/CreateRecordForm.svelte';
+	import type { State } from '../../types';
+	import { apiBaseUrl, http } from '../../common';
+	import Input from '../shared/Inputs/Input.svelte';
+	import CreateRecordForm from '../shared/Forms/CreateRecordForm.svelte';
 
-    export let onAddToStates: (state: State) => void;
+	export let onAddToStates: (state: State) => void;
 
-    const handleSubmitNewState = async (e: CustomEvent<SubmitEvent>) => {
-        if (!e.detail.target) return;
+	const handleSubmitNewState = async (e: CustomEvent<SubmitEvent>) => {
+		if (!e.detail.target) return;
 
-        const formEl = e.detail.target as HTMLFormElement;
-        const {parsedBody} = await http<State>({
-            input: `${apiBaseUrl}/states`,
-            body: {name: formEl.state.value},
-            method: 'POST'
-        });
+		const formEl = e.detail.target as HTMLFormElement;
+		const { parsedBody } = await http<State>({
+			input: `${apiBaseUrl}/states`,
+			body: { name: formEl.state.value },
+			method: 'POST'
+		});
 
-        if (parsedBody) {
-            onAddToStates(parsedBody);
-        }
-    };
+		if (parsedBody) {
+			onAddToStates(parsedBody);
+		}
+	};
 </script>
 
 <CreateRecordForm headingText="Nový stav plnění" on:submit={handleSubmitNewState}>
-    <Input label="Stav plnění" name="state"/>
+	<Input label="Stav plnění" name="state" />
 </CreateRecordForm>
